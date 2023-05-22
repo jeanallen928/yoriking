b12116f00ff863a138a001e28be0ffff5e758caa
jeanallen928 <123160000+jeanallen928@users.noreply.github.com>
2023-05-22 18:20:14 +0900

    :memo: add paths to articles/urls.py

--- articles/urls.py
+++ articles/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
+from articles import views
 
 urlpatterns = [
+    path("", views.ArticleView.as_view(), name="article-view"),
+    path("<int:article_id>/", views.ArticleDetailView.as_view(), name="article-detail-view"),
+    path("<int:article_id>/like/", views.LikeView.as_view(), name="like-view"),
+    path("comments/<int:comment_id>/", views.CommentView.as_view(), name="comment-view"),
 ]
