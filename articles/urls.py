38d3c42d2338298f8a627acac19041afd88c6191
jeanallen928 <123160000+jeanallen928@users.noreply.github.com>
2023-05-22 19:40:04 +0900

    :memo: add comment path to articles/urls.py

--- articles/urls.py
+++ articles/urls.py
@@ -4,6 +4,7 @@ from articles import views
 urlpatterns = [
     path("", views.ArticleView.as_view(), name="article-view"),
     path("<int:article_id>/", views.ArticleDetailView.as_view(), name="article-detail-view"),
+    path("<int:article_id>/comments/", views.CommentView.as_view(), name="comment-view"),
+    path("comments/<int:comment_id>/", views.CommentDetailView.as_view(), name="comment-detail-view"),
     path("<int:article_id>/like/", views.LikeView.as_view(), name="like-view"),
-    path("comments/<int:comment_id>/", views.CommentView.as_view(), name="comment-view"),
 ]
