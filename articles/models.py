4988bea962c23ade9a92e1bdd314917b7cacdba2
jeanallen928 <123160000+jeanallen928@users.noreply.github.com>
2023-05-22 22:23:12 +0900

    :memo: modify some names

--- articles/models.py
+++ articles/models.py
@@ -11,10 +11,10 @@ class Category(models.Model):
         ('ETC', '그외'),
     )
     
-    category = models.CharField("카테고리", choices=CATEGORIES, max_length=3)
+    food = models.CharField("음식", choices=CATEGORIES, max_length=3)
     
     def __str__(self):
-        return self.category
+        return self.food
     
 
 class Article(models.Model):
@@ -22,25 +22,24 @@ class Article(models.Model):
     user = models.ForeignKey(User, verbose_name="작성자", on_delete=models.CASCADE, related_name="articles")
     title = models.CharField("제목", max_length=50)
     content = models.TextField("내용")
-    created_at = models.DateTimeField("생성일", auto_now_add=True)
-    updated_at = models.DateTimeField("수정일", auto_now=True)
+    created_at = models.DateTimeField("작성시각", auto_now_add=True)
+    updated_at = models.DateTimeField("수정시각", auto_now=True)
     image = models.ImageField("이미지", upload_to="article/%Y/%m/", blank=True)
     likes = models.ManyToManyField(User, verbose_name="좋아요", blank=True, related_name='like_articles')
     category = models.ForeignKey(Category, verbose_name="카테고리", related_name="categorized_articles")
 
-    class Meta:
-        def __str__(self):
-            return self.title
+    
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
     user = models.ForeignKey(User, verbose_name="작성자", on_delete=models.CASCADE)
     article = models.ForeignKey(Article, verbose_name="게시글", on_delete=models.CASCADE, related_name="comments")
-    content = models.TextField("댓글 내용")
-    created_at = models.DateTimeField("댓글 생성일", auto_now_add=True)
-    updated_at = models.DateTimeField("댓글 수정일", auto_now=True)
+    content = models.TextField("내용")
+    created_at = models.DateTimeField("작성시각", auto_now_add=True)
+    updated_at = models.DateTimeField("수정시각", auto_now=True)
     
 
-    class Meta:
-        def __str__(self):
-            return self.content
+    def __str__(self):
+        return self.content
