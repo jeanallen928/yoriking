107f71e7d1b2059498a5943682fd3329c9c0a80e
jeanallen928 <123160000+jeanallen928@users.noreply.github.com>
2023-05-22 21:52:07 +0900

    :memo: modify articles/models.py
    added category model
    added some verbose names

--- articles/models.py
+++ articles/models.py
@@ -2,23 +2,31 @@ from django.db import models
 from users.models import User
 
 
-class Article(models.Model):
+class Category(models.Model):
     CATEGORIES = (
-        ('KO', '한식'),
-        ('CH', '중식'),
-        ('JP', '일식'),
-        ('WE', '양식'),
-        ('ET', '그외'),
+        ('KOR', '한식'),
+        ('CHN', '중식'),
+        ('JPN', '일식'),
+        ('WES', '양식'),
+        ('ETC', '그외'),
     )
     
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="articles")
+    category = models.CharField("카테고리", choices=CATEGORIES, max_length=3)
+    
+    def __str__(self):
+        return self.category
+    
+
+class Article(models.Model):
+    
+    user = models.ForeignKey(User, verbose_name="작성자", on_delete=models.CASCADE, related_name="articles")
     title = models.CharField("제목", max_length=50)
     content = models.TextField("내용")
     created_at = models.DateTimeField("생성일", auto_now_add=True)
     updated_at = models.DateTimeField("수정일", auto_now=True)
-    image = models.ImageField("이미지", null=True, upload_to="", blank=True)
-    like = models.ManyToManyField(User, blank=True, related_name='likes')
-    category = models.CharField("카테고리", choices=CATEGORIES, max_length=10)
+    image = models.ImageField("이미지", upload_to="article/%Y/%m/", blank=True)
+    likes = models.ManyToManyField(User, verbose_name="좋아요", blank=True, related_name='likes')
+    category = models.ForeignKey(Category, verbose_name="카테고리")
 
     class Meta:
         def __str__(self):
@@ -26,8 +34,8 @@ class Article(models.Model):
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(User, verbose_name="작성자", on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, verbose_name="게시글", on_delete=models.CASCADE, related_name="comments")
     content = models.TextField("댓글 내용")
     created_at = models.DateTimeField("댓글 생성일", auto_now_add=True)
     updated_at = models.DateTimeField("댓글 수정일", auto_now=True)
