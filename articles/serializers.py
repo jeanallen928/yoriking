3213b33942a4cd44d76a1b377845c7d7b64eb7d1
jeanallen928 <123160000+jeanallen928@users.noreply.github.com>
2023-05-23 14:29:56 +0900

    :bulb: add some annotations

--- articles/serializers.py
+++ articles/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from articles.models import Article, Comment
 
 
+# 댓글 조회
 class CommentSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
 
@@ -13,6 +14,7 @@ class CommentSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+# 게시글 전체 목록 조회 및 게시글 작성/수정
 class ArticleSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
     comment_count = serializers.SerializerMethodField()
@@ -32,6 +34,7 @@ class ArticleSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+# 게시글 상세보기
 class ArticleDetailSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
     comments = CommentSerializer(many=True)
@@ -52,6 +55,7 @@ class ArticleDetailSerializer(serializers.ModelSerializer):
         fields = "__all__"
         
 
+# 댓글 작성/수정
 class CommentCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
