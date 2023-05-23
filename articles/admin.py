92bcbe77aa81cba1bbcca11031bc9ed99e0bc741
jeanallen928 <123160000+jeanallen928@users.noreply.github.com>
2023-05-23 15:48:18 +0900

    :sparkles: comment CRUD
    added Comment to articles/admin.py

--- articles/admin.py
+++ articles/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from articles.models import Article, Category
+from articles.models import Article, Category, Comment
 
 
 admin.site.register(Article)
 admin.site.register(Category)
+admin.site.register(Comment)
