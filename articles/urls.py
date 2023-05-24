0c135db22852d240b3d02ecc7fd0bcee9817652c
jeanallen928 <123160000+jeanallen928@users.noreply.github.com>
2023-05-24 17:58:30 +0900

    :pencil2: modify names

--- articles/urls.py
+++ articles/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from articles import views
 
 urlpatterns = [
-    path("", views.ArticleView.as_view(), name="article-view"),
-    path("<int:article_id>/", views.ArticleDetailView.as_view(), name="article-detail-view"),
-    path("<int:article_id>/comments/", views.CommentView.as_view(), name="comment-view"),
-    path("comments/<int:comment_id>/", views.CommentDetailView.as_view(), name="comment-detail-view"),
-    path("<int:article_id>/like/", views.LikeView.as_view(), name="like-view"),
-    path('pagination/', views.ArticlesPaginationViewSet.as_view({'get': 'list'}), name='article-pagination'),
+    path("", views.ArticleView.as_view(), name="article_view"),
+    path("<int:article_id>/", views.ArticleDetailView.as_view(), name="article_detail_view"),
+    path("<int:article_id>/comments/", views.CommentView.as_view(), name="comment_view"),
+    path("comments/<int:comment_id>/", views.CommentDetailView.as_view(), name="comment_detail_view"),
+    path("<int:article_id>/like/", views.LikeView.as_view(), name="like_view"),
+    path('pagination/', views.ArticlesPaginationViewSet.as_view({'get': 'list'}), name='article_pagination'),
 ]
